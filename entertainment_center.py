c85a32c85e2ee71df9033493c2a99b3478550e3f
paulorsouza <paulor1809@gmail.com>
2017-12-20 21:02:59 -0200

    Resolve pylint hints

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,20 +2,21 @@ from media import Movie
 from fresh_tomatoes import open_movies_page
 
 def create_my_favorite_movies():
+    """Return a list of my favorite Movies"""
     return [
         Movie(
             "Star Wars: Episode IV - A New Hope",
-            "https://images-na.ssl-images-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,643,1000_AL_.jpg",
+            "https://images-na.ssl-images-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,643,1000_AL_.jpg", # pylint: disable=C0301
             "https://www.youtube.com/watch?v=oVxcQQXXxGQ"
         ),
         Movie(
             "Star Wars: The Force Awakens",
-            "https://images-na.ssl-images-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SY1000_CR0,0,677,1000_AL_.jpg",
+            "https://images-na.ssl-images-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SY1000_CR0,0,677,1000_AL_.jpg", # pylint: disable=C0301
             "https://www.youtube.com/watch?v=sGbxmsDFVnE"
         ),
         Movie(
             "Inception",
-            "https://images-na.ssl-images-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
+            "https://images-na.ssl-images-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg", # pylint: disable=C0301
             "https://www.youtube.com/watch?v=8hP9D6kZseM"
         )
     ]
--- media.py
+++ media.py
@@ -1,4 +1,6 @@
-class Movie:
+"""Why modules need docstring?"""
+class Movie(object):
+    """A model that represents movies"""
     def __init__(self, title, image_url, trailer_url):
         self.title = title
         self.poster_image_url = image_url
