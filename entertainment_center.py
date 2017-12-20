8652c98c52ec06dd233b92e0a2a80b413229fd42
paulorsouza <paulor1809@gmail.com>
2017-12-20 21:35:07 -0200

    Improve code removing movie information from entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,5 +1,16 @@
+"""This module is responsible to render a movie list"""
+
+import json
 from media import Movie
 from fresh_tomatoes import open_movies_page
+
+def create_movies_from_json(json_file):
+    """Transform a json file in a Movie instance"""
+    movie_list = []
+    for data in json.load(open(json_file)):
+        movie = Movie(data["title"], data["image"], data["trailer"])
+        movie_list.append(movie)
+    return movie_list
 
 def create_my_favorite_movies():
     """Return a list of my favorite Movies"""
@@ -21,4 +32,5 @@ def create_my_favorite_movies():
         )
     ]
 
-open_movies_page(create_my_favorite_movies())
+MY_FAVORITE_MOVIES = create_movies_from_json("my_favorite_movies.json")
+open_movies_page(MY_FAVORITE_MOVIES)
