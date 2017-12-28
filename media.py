13d57687262d5db161eb2f8852ae8bd90fcfcabc
paulorsouza <paulor1809@gmail.com>
2017-12-28 12:06:46 -0200

    Method definitions inside a class are surrounded by a single blank line.

--- media.py
+++ media.py
@@ -4,6 +4,7 @@
 class Movie(object):
     """A model that represents movies"""
     def __init__(self, title, image_url, trailer_url):
+
         self.title = title
         self.poster_image_url = image_url
         self.trailer_youtube_url = trailer_url
